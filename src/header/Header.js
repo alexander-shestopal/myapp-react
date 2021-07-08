611627da3b099b25d23850f7b71bf4df6e4fa33b
HHM\alex.shestopal <alex.shestopal@hhm.ch>
2021-07-08 08:36:33 +0300

    components React

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './Header.css';
 
-function App() {
-  return (
-    <div className="App">
+class Header extends React.Component {
+  render() {
+   return (
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,8 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
+ 
+  )
+}
 }
 
-export default App;
+export default Header;
---
+++ src/main/Main.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Main.css';
+
+class Main extends React.Component {
+  render() {
+   return (
+     <div className="container">
+        <main>
+          <article>
+            <header>
+              <h1>Article 1</h1>
+              <h2>Article 2</h2>
+            </header>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae praesentium amet quo consectetur quis dolorum cum facere consequatur minus est aspernatur a deserunt, optio non labore. Minus iste natus in.
+              </p>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae praesentium amet quo consectetur quis dolorum cum facere consequatur minus est aspernatur a deserunt, optio non labore. Minus iste natus in.
+              </p>
+          </article>
+        </main>
+ </div>
+  )
+}
+}
+
+
+export default Main;
