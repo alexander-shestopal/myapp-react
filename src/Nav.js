58a54b9561f1eaa36e6b920e226c057379d96d70
HHM\alex.shestopal <alex.shestopal@hhm.ch>
2021-07-08 15:06:52 +0300

    part. 2

--- src/Nav.js
+++ src/Nav.js
@@ -3,18 +3,33 @@ import React from 'react';
 
 
 class Nav extends React.Component {
-  render() {
-    let nav = this.props.nav;
+    constructor(props){
+        super(props)
+        this.state = {
+            title : "Navigation",
+            subtitle : "Main menu",
+            show: "show"
+        }
+    }
+    showNav  = ()=> {
+        this.setState({show: "hide"});
 
-   return (
-        <nav>
-            <ul>
-            {Object.keys(nav).map( elem =>{
-                return <li><a href = {nav[elem]}>{elem}</a></li>;
-            })}
-            </ul>
-        </nav>
-    );
-  }
+    }
+    render() {
+        let nav = this.props.nav;
+        return (
+            <nav>
+                <button type="button" onClick={this.showNav}>Show menu</button>
+                <h1>{this.state.title}</h1>
+                <h2>{this.state.subtitle}</h2>
+                <p>{this.state.show}</p>
+                <ul>
+                {Object.keys(nav).map( elem =>{
+                    return <li><a href = {nav[elem]}>{elem}</a></li>;
+                })}
+                </ul>
+            </nav>
+            );
+    }
 }
 export default Nav;
