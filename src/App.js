299c85c063b2bb3238e3a75df69fdf431f8bd4a9
HHM\alex.shestopal <alex.shestopal@hhm.ch>
2021-07-12 16:25:24 +0300

    form

--- src/App.js
+++ src/App.js
@@ -1,18 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: '',
+      text2: 555,
+      first_name : '',
+      age:0,
+    }
+  }
+  myInput = (e) => {
+    console.log(e.target.value);
+    console.log(e.target.name);
+    this.setState({[e.target.name]: e.target.value});
+  }
+  submitForm = (e) => {
+    e.preventDefault();
+
+  }
   render() {
-   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+    let a = '';
+    if(this.state.text) {
+      a = <p>Your enter text: {this.state.text}</p>
+    }
+    return (
+      <div>
+        <form onSubmit={this.submitForm}>
+          <input onChange={this.myInput} name="text"/>
+          <input onChange={this.myInput} type="number" name="age"/>
+          {a}
+          <input type="submit"/>
+          <p>Age:{this.state.age}</p>
+        </form>
+
       </div>
     );
   }
