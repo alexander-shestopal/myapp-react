1886997fe380d95e392a1e2a46ab7bf864b6286a
HHM\alex.shestopal <alex.shestopal@hhm.ch>
2021-07-12 14:05:33 +0300

    part3

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      text: 'Hello',
+      text2: 555,
+    }
+    this.showText = this.showText.bind(this);
+    this.showText2 = this.showText2.bind(this);
+    this.move = this.move.bind(this);
+  }
+  showText() {
+    console.log('work');
+    console.log(this)
+    this.setState({text: 'hi'});
+  }
+  showText2(e){
+    this.setState({text2:e.target.value});
+    console.log(e.target.value);
+  }
+  move(e){
+  
+    console.log(e.target.textContent);
+
+  }
   render() {
-   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
+    return (
+      <div>
+        <button onClick={this.showText}>Push1</button>
+        <input defaultValue={this.state.text} onInput={this.showText2}/>
+        <div onMouseMove={this.move}>lorem</div>
+        <p>{this.state.text}</p>
+        <p>{this.state.text2}</p>
+        <p>{this.state.text3}</p>
+ 
       </div>
     );
   }
