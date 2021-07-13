132e5874d7bc4cc65d033e6c5def84840de0963f
HHM\alex.shestopal <alex.shestopal@hhm.ch>
2021-07-13 16:41:19 +0300

    life-cycle

--- src/App.js
+++ src/App.js
@@ -6,37 +6,22 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      text: '',
-      text2: 555,
-      first_name : '',
-      age:0,
+      text: 'Hello',
     }
   }
-  myInput = (e) => {
-    console.log(e.target.value);
-    console.log(e.target.name);
-    this.setState({[e.target.name]: e.target.value});
-  }
-  submitForm = (e) => {
-    e.preventDefault();
+  // props -> state
+  // static getDerivedStateFromProps (props, state) {
+  //   return {text: props.text }
 
+  // }
+  componentDidMount(){
+    setTimeout( ()=>{this.setState({text: 'Component did mount'})},3000)
   }
   render() {
-    let a = '';
-    if(this.state.text) {
-      a = <p>Your enter text: {this.state.text}</p>
-    }
     return (
       <div>
-        <form onSubmit={this.submitForm}>
-          <input onChange={this.myInput} name="text"/>
-          <input onChange={this.myInput} type="number" name="age"/>
-          {a}
-          <input type="submit"/>
-          <p>Age:{this.state.age}</p>
-        </form>
-
-      </div>
+        <p>{this.state.text}</p>
+       </div>
     );
   }
 }
